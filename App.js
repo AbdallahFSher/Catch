3a70a4bed674a6a94f41f35f8358e8190a455309
Abdallah Sher <abdallah_sher@outlook.com>
2024-05-06 11:59:43 -0400

    Moved RSSEntry to a new folder

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { useFonts } from 'expo-font';
 import * as rssParser from 'react-native-rss-parser';
 import { useEffect, useState } from 'react';
 import './RSSEntry'
-import RSSEntry from "./RSSEntry";
+import RSSEntry from "./Components/RSSEntry";
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialBottomTabNavigator,  } from "react-native-paper/react-navigation";
