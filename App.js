24f90ff91c6eb58d860725be6b479b5ddf6a3356
Abdallah Sher <abdallah_sher@outlook.com>
2024-05-06 11:56:39 -0400

    added audio weeeeh
    Created an "item page" for each rss feed entry; gotta prototype this in illustrator or something to get a nice UI

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { ImageBackground, ScrollView, Text, StyleSheet, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { Image, ImageBackground, ScrollView, Text, StyleSheet, View } from 'react-native';
 import { Icon, Portal, Modal, FAB, Card, Divider, Button, PaperProvider, BottomNavigation, TextInput } from 'react-native-paper';
 import { useFonts } from 'expo-font';
 import * as rssParser from 'react-native-rss-parser';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './RSSEntry'
 import RSSEntry from "./RSSEntry";
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialBottomTabNavigator,  } from "react-native-paper/react-navigation";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Markdown from 'react-native-markdown-display';
+import { Audio } from 'expo-av'
 
 export default function App() {
   const Tab = createMaterialBottomTabNavigator();
   const Stack = createNativeStackNavigator();
+  const Drawer = createDrawerNavigator();
   const [fontsLoaded] = useFonts({'Beth Ellen' : require('./assets/fonts/BethEllen-Regular.ttf'),});
   const [idCounter, setidCounter] = useState(0);
   const feedURL = 'https://www.patreon.com/rss/theofficialpodcast?auth=x7Nf0Tjv_e2cTYJkH-tAPi55FQe8EHMV'
@@ -21,6 +26,7 @@ export default function App() {
   const [rssImage, setRSSImage] = useState(null)
   const [rssLastUpdated, setRSSLastUpdated] = useState(null)
   const [rssItems, setRSSItems] = useState([])
+  const [sound, setSound] = useState();
   const modalShow = () => setModalVisible(true);
   const modalHide = () => setModalVisible(false);
   const [URLs, setURLs] = useState([]);
@@ -35,83 +41,127 @@ export default function App() {
       .then((response) => response.text())
       .then((responseData) => rssParser.parse(responseData))
       .then((rss) =>
-      setRSSs([...RSSs,
-      {key: idCounter, title: rss.title, items: rss.items, image: rss.image}]));
-    setidCounter(idCounter + 1);
+        setRSSs([...RSSs,
+          {key: idCounter, title: rss.title, items: rss.items, image: rss.image}]));
+          setidCounter(idCounter + 1);
   };
-  const handleItemPress = key => {
-    setRSSImage(RSSs.find(obj => obj.key == key));
 
+  const itemCard = (title, description) => {
+    <View>
+      <Text>{title}</Text>
+      <Text>{description}</Text>
+    </View>
   }
 
-  const MainRoute = () => {
-      return(
-        <View>
+  async function playSound(url) {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync({uri: url}
+    );
+    setSound(sound);
 
-          <FeedRoute/>
-        </View>
-      )
-    }
-    ;
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
 
-  const ItemRoute = () =>
-    <View>
-      <ImageBackground source={rssImage}>
-      </ImageBackground>
-    </View>;
-
-  const FeedRoute = () =>
-    <View style={styles.mainContainer} id='deck'>
-      <ScrollView>
-      {
-        RSSs.map(function(rss){
-          return(
-            <RSSEntry title={rss.title} items={rss.items} image={rss.image}/>
-          )
-        })
+  useEffect(() => {
+    return sound
+    ? () => {
+      console.log('Unloading Sound');
+      sound.unloadAsync();
       }
-      </ScrollView>
-      <FAB
-        icon='plus'
-        style={styles.fab}
-        onPress={() => modalShow()}
-      />
-      <Portal>
-        <Modal visible={modalVisible} onDismiss={() => modalHide()} contentContainerStyle={styles.modal}>
-          <Text>Input your rss feed link, yo</Text>
-          <TextInput
-            label='URL'
-            value={inputURL}
-            onChangeText={inputURL => setInputURL(inputURL)}
-          />
-          <Button onPress={() =>handleURL(inputURL)}>Add Feed</Button>
-        </Modal>
-      </Portal>
-    </View>;
+    : undefined;
+  }, [sound]);
+
+  function ItemRoute({route, navigation}){
+    const {title, items, image} = route.params;
+    return(
+        <View>
+          <ImageBackground source={image} blurRadius={10}>
+            <Text style={styles.itemTitle}>{title}</Text>
+            <ScrollView>
+              {
+                items.map(function(item) {
+                  return (
+                    <Card>
+                      <Card.Title title={item.title}/>
+                      <Card.Content>
+                        <Text>
+                          {item.description}
+                        </Text>
+                      </Card.Content>
+                      <Card.Actions>
+                        <Button onPress={() => playSound(item["enclosures"][0].url)}>Play</Button>
+                      </Card.Actions>
+                    </Card>
+                  )
+                })
+              }
+            </ScrollView>
+          </ImageBackground>
+          </View>
+    )
+  }
+
+  function displayLogo(){
+    return (
+        <Image source={{uri: './dog.jpg'}}/>
+    )
+  }
+
+  function Feed({navigation}){
+    return(
+      <View style={styles.mainContainer} id='deck'>
+        <ScrollView>
+          {
+            RSSs.map(function(rss){
+              return(
+                <RSSEntry title={rss.title} items={rss.items} image={rss.image} onPress={() => navigation.push('ItemPage', {title: rss.title, items: rss.items, image: rss.image})}/>
+              )})
+          }
+        </ScrollView>
+        <FAB
+          icon='plus'
+          style={styles.fab}
+          onPress={() => modalShow()}
+        />
+        <Portal>
+          <Modal visible={modalVisible} onDismiss={() => modalHide()} contentContainerStyle={styles.modal}>
+            <Text>Input your rss feed link, yo</Text>
+            <TextInput
+              label='URL'
+              value={inputURL}
+              onChangeText={inputURL => setInputURL(inputURL)}
+            />
+            <Button onPress={() =>handleURL(inputURL)}>Add Feed</Button>
+          </Modal>
+        </Portal>
+      </View>
+    )
+  }
+  function FeedRoute({navigation}) {
+    const handleItemPress = key => {
+      setRSSImage(RSSs.find(obj => obj.key == key));
+      navigation.navigate('ItemPage', {title: key.title, items: key.items, image: key.image})
+    }
+    return(
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name='FeedPage' component={Feed}/>
+          <Stack.Screen name='ItemPage' component={ItemRoute}/>
+        </Stack.Navigator>
+    )
+  };
 
   const LibraryRoute = () => <Text>Library</Text>;
   const SettingsRoute = () => <Text>Settings</Text>;
 
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'feed', title: 'Feed', focusedIcon: 'rss-box'},
-    { key: 'library', title: 'Library', focusedIcon: 'bookshelf' },
-    { key: 'settings', title: 'Settings', focusedIcon: 'cog' },
-  ]);
-
-  const renderScene = BottomNavigation.SceneMap({
-    feed: FeedRoute,
-    library: LibraryRoute,
-    settings: SettingsRoute,
-  });
   
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Stack.Screen name='Main' component={FeedRoute} options={{title: 'Feed'}}/>
-          <Stack.Screen name='ItemPage' component={ItemRoute}/>
-        </Tab.Navigator>
+        <Drawer.Navigator>
+          <Drawer.Screen name='Main' component={FeedRoute} options={{title:'Feed'}}/>
+        </Drawer.Navigator>
       </NavigationContainer>
 
     </PaperProvider>
@@ -163,5 +213,23 @@ const styles = StyleSheet.create({
     margin: 16,
     right: 0,
     bottom: 0,
+  },
+  itemTitle: {
+    height: '10%',
+    color: 'white',
+    fontSize: 25,
+    paddingTop: 30,
+    fontFamily: 'AlNile-Bold',
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: 'transparent'
+  },
+  itemCard: {
+    height: '50%',
+    width: '50%'
+  },
+  itemDescription: {
+    fontSize: 10,
+    color: 'black'
   }
 });
